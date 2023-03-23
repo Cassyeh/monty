f5f2b781f1840d6dcd9c7ab1b0aea4703923ce22
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-03-23 16:49:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	stack_init(&head);
 	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 	process_file(argv[1], &head);
