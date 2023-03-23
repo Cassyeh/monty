001719471f7813ea73411db698fc370dd264421d
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-03-23 17:59:59 +0100

    Update command_func3.c

--- command_func3.c
+++ command_func3.c
@@ -25,3 +25,28 @@ void _mod(stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	(*stack)->n %= tmp;
 }
+/**
+ * _pchar - prints the char at the top of the stack
+ * @stack: pointer to pointer to top of stack
+ * @line_number: value of node
+ * Return: nothing
+ **/
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	int tmp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = (*stack)->n;
+	if (!(tmp >= 0 && tmp <= 127))
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range", line_number);
+		exit(EXIT_FAILURE);
+	}
+	putchar(tmp);
+	putchar('\n');
+}
