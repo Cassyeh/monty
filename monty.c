f31b1e13bc6e15eaa7aff7fa3edbd18758427469
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-03-23 17:31:49 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,12 +1,13 @@
 #include "monty.h"
 
+glob_t global;
+
 /**
  * stack_init - initialize all the things
  * @head: top of stack data structure
  **/
 void stack_init(stack_t **head)
 {
-	glob_t global;
 	*head = NULL;
 	global.top = head;
 }
@@ -18,7 +19,6 @@ void stack_init(stack_t **head)
  **/
 void free_all(void)
 {
-	glob_t global;
 	stack_t *tmp1, *tmp2 = NULL;
 
 	tmp1 = *(global.top);
